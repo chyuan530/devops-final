bfe38ae23d378ec048a4f66ee84d7c94abb594ec
Chris Yuan <chyuan530@gmail.com>
2021-09-18 18:32:20 -0700

    move around jenkins install files

--- jenkins/jenkins_install.sh
+++ jenkins/jenkins_install.sh
@@ -29,14 +29,14 @@ sudo docker run --name jenkins-docker --rm --detach \
 # scp ...
 
 # build and run custom jenkins image
-sudo docker build -t myjenkins-blueocean:1.1 .
+sudo docker build -t myjenkins-blueocean:1.2 .
 sudo docker run --name jenkins-blueocean --rm --detach \
   --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
   --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
   --publish 8080:8080 --publish 50000:50000 \
   --volume ${HOME}/jenkins_home:/var/jenkins_home \
   --volume jenkins-docker-certs:/certs/client:ro \
-  myjenkins-blueocean:1.1
+  myjenkins-blueocean:1.2
 #  --volume jenkins_data:/var/jenkins_home \  
 
 # access init jenkins password on docker container
